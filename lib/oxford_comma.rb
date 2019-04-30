c79664c2fab2caae5d08244de2b2b16735f9d528
Iskra B <ibatistapoblete@gmail.com>
2019-04-30 01:26:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
+#require 'pry'
 def oxford_comma(array)
+  if array.length == 1
+    array.join(" ")
+  elsif array.length == 2
+    return array.join(" and ")
+  else array.length >= 3
+    new_arr = array.join(", ")
+    new_arr.split(" ").insert(-3, "and").join(" ")
 
+    #array
+    #array[-1].insert(0, "and ")
+    #binding.pry
+  end
+  #array.join(", ")
 end
+
+#array.join(", ").split("").insert(-2, "and").split(" ")
